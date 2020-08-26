c95a4c693cc6833391e9cd6a2fe3288f0596dc8c
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-08-26 18:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function iterativeLog(array) {
   array.forEach(`${index}: ${element}`);
 };
+
+function iterate(callback) {
+  var iterateArray = [1,2,3,4];
+  iterateArray.forEach(callback)
+}
+return iterateArray;
